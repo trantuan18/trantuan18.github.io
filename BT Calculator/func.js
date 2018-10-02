3d29e018c8e1e8f4e4d27d9b6a6dbd2d3d5067a9
web <web@iMac-7.local>
2018-10-02 17:16:19 +0700

    02/10

---
+++ BT Calculator/func.js
@@ -0,0 +1,103 @@
+var s='';
+// var no1= document.getElementById('b1');
+// var no2= document.getElementById('b2');
+// var no3= document.getElementById('b3');
+// var no4= document.getElementById('b4');
+// var no5= document.getElementById('b5');
+// var no6= document.getElementById('b6');
+// var no7= document.getElementById('b7');
+// var no8= document.getElementById('b8');
+// var no9= document.getElementById('b9');
+// var no0= document.getElementById('b0');
+function buttonon1(){
+	x='1';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttonon2(){
+	x='2';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttonon3(){
+	x='3';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttonon4(){
+	x='4';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttonon5(){
+	x='5';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttonon6(){
+	x='6';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttonon7(){
+	x='7';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttonon8(){
+	x='8';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttonon9(){
+	x='9';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttonon0(){
+	x='0';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttononclear(){
+	s='';
+	document.getElementById("result").innerText = s; 
+	document.getElementById("result1").innerText = '';
+}
+function buttonondel(){
+	x=s.slice(0,s.length-1);
+	s=x;
+	document.getElementById("result").innerText = s; 
+}
+function buttononcham(){
+	x='.';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttononpercent(){
+	x='%';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttononmultiplication(){
+	x='*';s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttonondivi(){
+	x='/';s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttononplus(){
+	x='+';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttononsub(){
+	x='-';
+	s=s+x;
+	document.getElementById("result").innerText = s; 
+}
+function buttononfinish(){
+	document.getElementById("result1").innerText = eval(s);
+
+}
