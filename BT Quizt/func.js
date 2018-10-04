9f3182eaec97e24c5008b130120c16eb6947a7c3
web <web@iMac-7.local>
2018-10-04 17:34:57 +0700

    04/10

---
+++ BT Quizt/func.js
@@ -0,0 +1,41 @@
+var no1{
+	Quest  : 'Một sào Bắc bộ, Trung bộ bằng bao nhiêu m2 mét vuông ? ' ,
+	A : 360 ,
+	B : 500 , 
+	C : 1000 ,
+	D : 720 ,
+	tr: 'A'
+}
+var no2{
+	Quest  : 'Cải cách ruộng đất ở miền Bắc Việt Nam diễn ra trong thời gian nào ? ' ,
+	A : '1953 - 1956' ,
+	B : '1990 - 1993' , 
+	C : '1945 - 1948' ,
+	D : '1993 - 1995' ,
+	tr: 'A'
+}
+var no3{
+	Quest  : 'Vụ chiêm diễn ra vào thời gian nào trong năm  ? ' ,
+	A : 'Đông - Xuân ' ,
+	B : 'Hạ - Thu ' , 
+	C : 'Thu - Đông ' ,
+	D : 'Xuân - hạ ' ,
+	tr: 'A'
+}
+var no4{
+	Quest  : 'Âm lịch( Nông lịch ) được tạo ra dựa trên chu kì nào  ? ' ,
+	A : ' Mặt trăng ' ,
+	B : ' Mặt trời ' , 
+	C : ' Vụ thu hoạch nông sản ' ,
+	D : ' Chòm sao ' ,
+	tr: 'A'
+}
+var no5{
+	Quest  : ' Tết nguyên tiêu diễn ra vào tháng mấy ? ' ,
+	A : ' Tháng 1 ' ,
+	B : ' Tháng 12 ' , 
+	C : ' Tháng 7 ' ,
+	D : ' Tháng 3 ' ,
+	tr: 'A'
+}
+
