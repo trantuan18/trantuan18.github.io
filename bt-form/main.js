31a85a4bb43dae7075c6d8cbe783ca44ba0bd1eb
TranTuan <tqtrantuan@gmail.com>
2018-10-26 14:48:42 +0700

    BT img có responsive

--- bt-form/main.js
+++ bt-form/main.js
@@ -1,8 +1,10 @@
+//lấy url 
 let url = window.location.href;
+
 let string = decodeURIComponent(url);
 let start = 1 + string.indexOf('?');
 let str = string.slice(start);
-
+//tách lấy vị trí từng phần thông tin điền vào
 let start1 = [];
 let end = [];
 for (let i = 0; i <= str.length; i++) {
@@ -13,13 +15,13 @@ for (let i = 0; i <= str.length; i++) {
         end.push(i)
     };
 };
-
+//tách từng chuỗi 
 let name = str.slice(start1[0] + 1, end[0]);
 let date = str.slice(start1[1] + 1, end[1]);
 let contact = str.slice(start1[2] + 1, end[2]);
 let mail = str.slice(start1[3] + 1, end[3]);
 let sex = str.slice(start1[5] + 1, end[5]);
-
+// chỉnh lại tên 
 let trueName = '';
 for (let i = 0; i < name.length; i++) {
     if (name[i] == '+') {
@@ -28,12 +30,12 @@ for (let i = 0; i < name.length; i++) {
         trueName += name[i];
     };
 };
-
+// doi lai ngay thang nam 
 let y = date.slice(0, 4);
 let m = date.slice(5, 7);
 let d = date.slice(8, 10);
 let trueDate = d + '-' + m + '-' + y;
-
+//Xuất thông tin
 $("#names").text('Họ và tên: ' + trueName);
 $("#date").text('Ngày sinh: ' + trueDate);
 $("#contact").text('Số điện thoại: ' + contact);
