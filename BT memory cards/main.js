f653c01493f3cd804069494eff4775c044b26fdb
trantuan18 <tqtrantuan@gmail.com>
2018-11-16 15:22:43 +0700

    'project-design'

--- BT memory cards/main.js
+++ BT memory cards/main.js
@@ -7,9 +7,6 @@ function flipBack() {
 function flipFront() {
 
 }
-$('.back').click(function() {
-    this.style.transform = 'rotateY(180 deg)';
-    // $('.back')[0].style.transform = 'rotateY(180 deg)';
-    // $('.front')[0].style.transform = 'rotateY(0 deg)';
-    this.style.display = 'none';
+$('.card').on('click',function() {
+    
 })
